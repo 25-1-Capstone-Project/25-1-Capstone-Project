aa2e802e7d7f677a36428d121fcc2762086d3a47
dosun15 <158137636+dosun15@users.noreply.github.com>
2025-04-30 01:12:48 +0900

    20250430 SkillUI 추가
    Skill UI 추가
    - UIManager, UICanvas 프리팹 수정
    - SkillUI 스크립트 추가
    - UIManager, PlayerScript 스크립트 수정
    
    TestScene환경에서 테스트, 나머지 씬에서도 일괄 적용되도록 UI관련 프리팹 수정해둠
    PlayerScript 스크립트 건드림(playerInit currentSkill 설정하는 부분에서 UI매니저 호출 → 이미지 수정되도록)

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -6,17 +6,16 @@ public class UIManager : Singleton<UIManager>
   
     public PlayerStatUI playerStatUI;
     public ParryStackUI parryStackUI;
-  
+    public SkillUI skillUI;
 
     protected override void Awake()
     {
-        base.Awake(); 
-        
+        base.Awake();
     }
 
     private void LateUpdate()
     {
-        playerStatUI.UI_ParryCooldownUpdate();
+        //playerStatUI.UI_ParryCooldownUpdate();
        // playerStatUI.UI_ParryStackUpdate();
         playerStatUI.UI_HPBarUpdate();
     }
