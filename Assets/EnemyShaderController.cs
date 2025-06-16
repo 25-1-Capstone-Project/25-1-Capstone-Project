add785d5798ff7bfa4b4a28bd831b26b0b5667d0
DESKTOP-KU2CFGR\hurch <hurchanqwer>
2025-06-16 16:44:21 +0900

    Shader 오류 발견해서 수정 bacher 고침

--- Assets/EnemyShaderController.cs
+++ Assets/EnemyShaderController.cs
@@ -2,23 +2,35 @@ using UnityEngine;
 
 public class EnemyShaderController : MonoBehaviour
 {
-    Material enemyMaterial;
+    private Material enemyMaterial;
     [SerializeField] private float outlineThickness = 2f;
-    public void Start()
+
+    private void Start()
     {
-        enemyMaterial = GetComponent<SpriteRenderer>().material;
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        enemyMaterial = spriteRenderer.material;
+
+        //텍스처 크기 기반으로 텍셀 사이즈 계산
+        Texture mainTex = enemyMaterial.mainTexture;
+        if (mainTex != null)
+        {
+            Vector2 texelSize = new Vector2(1f / mainTex.width, 1f / mainTex.height);
+            enemyMaterial.SetVector("_CustomTexelSize", texelSize);
+        }
     }
-    private void OnDisable() {
-        enemyMaterial.SetFloat("_OutlineColor", 0);
-    } 
-    
+
+    private void OnDisable()
+    {
+        enemyMaterial.SetFloat("_OutlineThickness", 0);
+    }
+
     public void OnOutline()
     {
-        enemyMaterial.SetFloat("_OutlineColor", outlineThickness);
-
+        enemyMaterial.SetFloat("_OutlineThickness", outlineThickness);
     }
+
     public void OffOutline()
     {
-        enemyMaterial.SetFloat("_OutlineColor", 0);
+        enemyMaterial.SetFloat("_OutlineThickness", 0);
     }
 }
