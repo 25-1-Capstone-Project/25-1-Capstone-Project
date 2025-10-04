569b56c24b6bed6122a9a86a59e26e844be27dbf
DESKTOP-KU2CFGR\hurch <hurchanqwer>
2025-10-04 18:01:40 +0900

    Astar Add

---
+++ Assets/AIAgent.cs
@@ -0,0 +1,29 @@
+using Pathfinding;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class AIAgent : MonoBehaviour
+{
+    private AIPath path;
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private Transform target;
+    [SerializeField] private float stopdistance = 2f;
+    private float distanceToTarget;
+    void Start()
+    {
+        path = GetComponent<AIPath>();
+    }
+    void Update()
+    {
+        path.maxSpeed = speed;
+        distanceToTarget = Vector2.Distance(transform.position, target.position);
+        if (distanceToTarget < stopdistance)
+        {
+            path.destination = transform.position;
+        }
+        else
+        {
+            path.destination = target.position;
+        }
+    }
+}
