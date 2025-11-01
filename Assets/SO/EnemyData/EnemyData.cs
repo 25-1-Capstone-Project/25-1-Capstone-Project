76fa5b63fd3c9eb0fdc35e9a9d375ab13a046afb
DESKTOP-07V7LEM\hurch <hurchanqwer@naver.com>
2025-11-01 16:59:03 +0900

    무스무스 추가

--- Assets/SO/EnemyData/EnemyData.cs
+++ Assets/SO/EnemyData/EnemyData.cs
@@ -6,26 +6,26 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemyData", menuName = "Enemy/EnemyData")]
 public class EnemyData : EnemyDataBase
 {
-    public EEnemyType eEnemyType;
-    public ECloseAttackType closeAttackType;
-    public ELongAttackType longAttackType;
-    public ESpecialAttackType specialAttackType;
+    // public EEnemyType eEnemyType;
+    // public ECloseAttackType closeAttackType;
+    // public ELongAttackType longAttackType;
+    // public ESpecialAttackType specialAttackType;
 
     
     public override void AttackPatternSet(int index)
     {
-        switch (eEnemyType)
-        {
-            case EEnemyType.CloseRange:
-                attackPattern = EnemyManager.Instance.CloseEnemyAttackPatterns[(int)closeAttackType];
-                break;
-            case EEnemyType.LongRange:
-                attackPattern = EnemyManager.Instance.LongenemyAttackPatterns[(int)longAttackType];
-                break;
-            case EEnemyType.SpecialAttack:
-                attackPattern = EnemyManager.Instance.SpecialEnemyAttackPatterns[(int)specialAttackType];
-                break;
-        }
+        // switch (eEnemyType)
+        // {
+        //     case EEnemyType.CloseRange:
+        //         attackPattern = EnemyManager.Instance.CloseEnemyAttackPatterns[(int)closeAttackType];
+        //         break;
+        //     case EEnemyType.LongRange:
+        //         attackPattern = EnemyManager.Instance.LongenemyAttackPatterns[(int)longAttackType];
+        //         break;
+        //     case EEnemyType.SpecialAttack:
+        //         attackPattern = EnemyManager.Instance.SpecialEnemyAttackPatterns[(int)specialAttackType];
+        //         break;
+        // }
     }
 
 
